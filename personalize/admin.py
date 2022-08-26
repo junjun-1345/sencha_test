fb2a7074a2baa8046e50f3e8fc9114982270e1bd
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2022-08-26 18:11:43 +0900

    データベース完成　名前、ベース、症状

--- personalize/admin.py
+++ personalize/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Tea
+
 # Register your models here.
+admin.site.register(Tea)
--- personalize/models.py
+++ personalize/models.py
@@ -2,4 +2,27 @@ from django.db import models
 
 
 # Create your models here.
+class Tea(models.Model):
+    name = models.IntegerField(verbose_name='名前', blank=True, null=True)
+    base = models.CharField(verbose_name='ベース', max_length=50, blank=True, null=True)
+    symptoms1 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms2 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms3 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms4 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms5 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms6 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms7 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms8 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms9 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms10 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms11 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms12 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms13 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms14 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
+    symptoms15 = models.CharField(verbose_name='症状', max_length=50, blank=True, null=True)
 
+    class Meta:
+        verbose_name_plural = 'Tea'
+
+    def __int__(self):
+        return self.name
